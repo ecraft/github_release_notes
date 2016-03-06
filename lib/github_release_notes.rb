3547a032db6768618788b5dc79cb4437a3af7adf
Olle Jonsson <olle.jonsson@gmail.com>
2016-03-06 17:13:07 +0100

    Docblocks

--- lib/github_release_notes.rb
+++ lib/github_release_notes.rb
@@ -9,7 +9,9 @@ require 'active_support/core_ext/hash/slice'
 require 'active_support/core_ext/object/blank'
 require 'rake/clean'
 
+# Github Release Notes generator
 module GithubReleaseNotes
+  # Application-specific error
   Error = Class.new(StandardError)
 end
 require 'github_release_notes/configuration'
