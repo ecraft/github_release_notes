9974a34e56d54d10a4a41d944710a406f7cd249b
Olle Jonsson <olle.jonsson@gmail.com>
2016-07-02 17:44:33 +0200

    Spec coverage bugfix: include SimpleCov first

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'github_release_notes'
 require 'simplecov'
 SimpleCov.start do
   add_filter '/spec/'
 end
+require 'github_release_notes'
