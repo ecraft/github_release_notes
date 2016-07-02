2eecdd19c4cc623ed0e8fda5e1f761f62c21a0d5
Olle Jonsson <olle.jonsson@gmail.com>
2016-07-02 19:12:21 +0200

    Refactor formatter classes

--- lib/github_release_notes/html_formatter.rb
+++ lib/github_release_notes/html_formatter.rb
@@ -1,36 +1,43 @@
 require 'pathname'
 module GithubReleaseNotes
   class HtmlFormatter < Formatter
-    attr_reader :rendered_html, :full_html
+    attr_reader :rendered_html
     attr_accessor :rendered_markdown
 
     def call
-      if configured_to_write_to(config.html_output)
-        format_html
+      return if must_skip_rendering!
 
-        File.open(config.html_output, 'w') do |f|
-          f.write(full_html)
-        end
-        logger.info { "Generated #{config.html_output}" }
-      else
-        logger.debug "Skipping HTML output. #{config.html_output}"
+      File.open(config.html_output, 'w') do |f|
+        f.write(format)
       end
+      logger.info { "Generated #{config.html_output}" }
     end
+
+    private
 
     def validate_options!
       raise Error, ANSI.red {
         ':html_output must be set to a writable path.' \
-        ". :html_output: #{config.html_output.inspect}"
-      } unless configured_to_write_to(config.html_output)
+        " Current value: #{config.html_output.inspect}"
+      } if config.html_output.present? && !configured_to_write_to(config.html_output)
     end
 
-    def format_html
-      @full_html ||= begin
-        format_releases_in_html
-        html_preamble
-        html_epilogue
-        html_content
+    def must_skip_rendering!
+      if rendered_markdown.blank?
+        logger.debug "No Markdown inserted. Can not render HTML output."
+        true
+      elsif !configured_to_write_to(config.html_output)
+        true
+      else
+        false
       end
+    end
+
+    def format
+      format_releases_in_html
+      html_preamble
+      html_epilogue
+      html_content
     end
 
     def format_releases_in_html
@@ -39,24 +46,16 @@ module GithubReleaseNotes
 
     def html_preamble
       data = config.preamble_template_data
-      @preamble = ERB.new(File.read(preamble_template_path)).result(binding)
+      @preamble = ERB.new(File.read(templates_path + 'html_preamble.html.erb')).result(binding)
     end
 
     def html_epilogue
       data = config.epilogue_template_data
-      @epilogue = ERB.new(File.read(epilogue_template_path)).result(binding)
+      @epilogue = ERB.new(File.read(templates_path + 'html_epilogue.html.erb')).result(binding)
     end
 
     def html_content
       "#{preamble}#{rendered_html}#{epilogue}".force_encoding('UTF-8')
-    end
-
-    def preamble_template_path
-      templates_path + 'html_preamble.html.erb'
-    end
-
-    def epilogue_template_path
-      templates_path + 'html_epilogue.html.erb'
     end
   end
 end
--- lib/github_release_notes/markdown_formatter.rb
+++ lib/github_release_notes/markdown_formatter.rb
@@ -4,16 +4,10 @@ module GithubReleaseNotes
     attr_reader :rendered_markdown
 
     def call
-      if configured_to_write_to(config.markdown_output)
-        format_markdown
-
-        File.open(config.markdown_output, 'w') do |f|
-          f.write(rendered_markdown)
-        end
-        logger.info { "Generated #{config.markdown_output}" }
-      else
-        logger.debug "Skipping Markdown output. #{config.markdown_output}"
+      File.open(config.markdown_output, 'w') do |f|
+        f.write(format_markdown)
       end
+      logger.info { "Generated #{config.markdown_output}" }
     end
 
     private
@@ -21,22 +15,18 @@ module GithubReleaseNotes
     def validate_options!
       raise Error, ANSI.red {
         ':markdown_output must be set to a writable path.' \
-        ". :markdown_output: #{config.markdown_output.inspect}"
-      } unless configured_to_write_to(config.markdown_output)
+        " Current value: #{config.markdown_output.inspect}"
+      } if config.markdown_output.blank? || !configured_to_write_to(config.markdown_output)
     end
 
     def format_markdown
       @rendered_markdown ||= begin
-        release_template = File.read(release_template_path)
+        release_template = File.read(templates_path + 'release.md.erb')
 
         releases.map do |release|
           ERB.new(release_template).result(binding)
         end.join('').force_encoding('UTF-8')
       end
-    end
-
-    def release_template_path
-      templates_path + 'release.md.erb'
     end
   end
 end
